063c2b5dd341274399652e7b50a29b78f73b27a4
ReeceStevens <reecestevens24@gmail.com>
2017-12-13 13:20:00 -0600

    Avoid memory leak by producing single frames in python

--- cam_animation.py
+++ cam_animation.py
@@ -15,6 +15,8 @@ from train import DataClassifier
 from predict import get_model_predictions_for_npz
 from visualize import plot_row_item
 
+# Debug purposes only
+# from pympler import muppy, summary 
 
 def cam_weighted_image(model, image_path, character_idx):
     pixels = np.load(image_path)['pixels']
@@ -23,64 +25,86 @@ def cam_weighted_image(model, image_path, character_idx):
                         seed_input=pixels)
     return np.uint8(pixels*np.dstack([cam]*3))
 
+def make_cam_plot(model, weight, image_path, cam_path, data_classifier):
+    path_head, npz_name = os.path.split(image_path)
+    _, character_name = os.path.split(path_head)
+
+    # print("generating CAM image")
+    character_idx = data_classifier.one_hot_index(character_name)
+    cam = cam_weighted_image(model, image_path, character_idx)
+
+    fig = plt.figure()
+    inner = gridspec.GridSpec(2, 1, wspace=0.05, hspace=0, height_ratios=[5, 1.2])
+    image_ax = plt.Subplot(fig, inner[0])
+    labels_ax = plt.Subplot(fig, inner[1])
+    # print("Getting model predictions")
+    character_name_to_probability = get_model_predictions_for_npz(model,
+                                                                  data_classifier,
+                                                                  character_name,
+                                                                  npz_name)
+    top_character_probability = sorted(character_name_to_probability.items(),
+                                       key=lambda item_tup: item_tup[1],
+                                       reverse=True)[:3]
+    top_character_names, top_character_probabilities = zip(*top_character_probability)
+
+    # print("Plotting CAM image")
+    plot_row_item(image_ax, labels_ax, cam, top_character_names, top_character_probabilities)
+    weight_idx = os.path.basename(weight).split('.')[1]
+    labels_ax.set_xlabel(npz_name)
+    image_ax.set_title(model_name + ', epoch ' + weight_idx)
+
+    fig.add_subplot(image_ax)
+    fig.add_subplot(labels_ax)
+
+    # print("Saving CAM image")
+    plt.savefig(os.path.join(cam_path, 'cam_{}.png'.format(weight_idx)))
+    plt.close(fig)
+    # print("Finished.")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generate an animation of class-activation maps")
-    parser.add_argument('--weight-directory', required=True,
-                        help="Directory containing the model weight files")
+    # parser.add_argument('--weight-directory', required=True,
+    #                     help="Directory containing the model weight files")
+    parser.add_argument('--weight-file', required=True,
+                        help="Model weight file")
     parser.add_argument('--data-directory', required=True,
                         help="Directory containing the input *.npz images")
     parser.add_argument('--image-path', required=True,
                         help="A specific image path to plot CAM for.")
     parser.add_argument('--cam-path', required=True,
                         help="Directory for storing CAM plots.")
-    parser.add_argument('--weight-limit', required=False, type=int)
+    # parser.add_argument('--weight-limit', required=False, type=int)
     args = parser.parse_args(sys.argv[1:])
 
-    print('Building data classifier')
+    # print('Building data classifier')
     data_classifier = DataClassifier(args.data_directory)
-    print("...finished!")
+    # print("...finished!")
 
-    path_head, npz_name = os.path.split(args.image_path)
-    _, character_name = os.path.split(path_head)
+    model_name = os.path.basename(os.path.dirname(args.weight_file))
 
-    print('Beginning to sort weights in {}'.format(os.path.join(args.weight_directory, '*.h5')))
-    weights = sorted(list(glob(os.path.join(args.weight_directory, '*.h5'))))
+    # print("loading model")
+    model = load_model(args.weight_file)
+    make_cam_plot(model, args.weight_file, args.image_path, args.cam_path, data_classifier)
 
-    model_name = os.path.basename(args.weight_directory)
+    # path_head, npz_name = os.path.split(args.image_path)
+    # _, character_name = os.path.split(path_head)
+
+    # print('Beginning to sort weights in {}'.format(os.path.join(args.weight_directory, '*.h5')))
+    # weights = sorted(list(glob(os.path.join(args.weight_directory, '*.h5'))))
 
 
-    print("Beginning CAM plots")
-    for idx, weight in enumerate(tqdm.tqdm(weights[:args.weight_limit], unit='weights')):
-        if args.weight_limit and idx >= args.weight_limit:
-            break
-        model = load_model(weight)
 
-        character_idx = data_classifier.one_hot_index(character_name)
-        cam = cam_weighted_image(model, args.image_path, character_idx)
+    # print("Beginning CAM plots")
 
-        fig = plt.figure()
-        inner = gridspec.GridSpec(2, 1, wspace=0.05, hspace=0, height_ratios=[5, 1.2])
-        image_ax = plt.Subplot(fig, inner[0])
-        labels_ax = plt.Subplot(fig, inner[1])
-        character_name_to_probability = get_model_predictions_for_npz(model,
-                                                                      data_classifier,
-                                                                      character_name,
-                                                                      npz_name)
-        top_character_probability = sorted(character_name_to_probability.items(),
-                                           key=lambda item_tup: item_tup[1],
-                                           reverse=True)[:3]
-        top_character_names, top_character_probabilities = zip(*top_character_probability)
+    # start = summary.summarize(muppy.get_objects())
 
-        plot_row_item(image_ax, labels_ax, cam, top_character_names, top_character_probabilities)
-        labels_ax.set_xlabel(npz_name)
-        image_ax.set_title(model_name + ', epoch ' + os.path.basename(weight).split('.')[1])
+    # for idx, weight in enumerate(tqdm.tqdm(weights[:args.weight_limit], unit='weights')):
+    #     # cursor = summary.summarize(muppy.get_objects())
+    #     # summary.print_(summary.get_diff(start, cursor))
+    #     if args.weight_limit and idx >= args.weight_limit:
+    #         break
 
-        fig.add_subplot(image_ax)
-        fig.add_subplot(labels_ax)
-
-        idx_str = str(idx) if idx > 9 else '0'+str(idx)
-        plt.savefig(os.path.join(args.cam_path, 'cam_{}.png'.format(idx_str)))
-        plt.close(fig)
-        del cam
-        del fig
-        del model
+    #     del top_character_names, top_character_probabilities, top_character_probability, character_name_to_probability
+    #     del cam
+    #     del fig
+    #     del model
